1dc9e76205deadde4d8b6092d5662c8b14165c7d
Marci Prescott <mjprescott@me.com>
2025-01-19 16:53:40 -0700

    Added icon

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const inputBtn = document.getElementById("input-btn");
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(localStorage.getItem(myLeads));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  // Save the myLeads array to localStorage
+  // PS: remember JSON.stringify()
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
+
+  // To verify that it works:
+  console.log(localStorage.getItem("myLeads"));
 });
+
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
